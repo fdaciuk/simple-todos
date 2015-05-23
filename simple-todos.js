1816fca12cb512d31ae5633417e675dbf635d327
Fernando Daciuk <f.daciuk@gmail.com>
2015-05-23 10:39:18 -0300

    Add private funcionality to all methods

--- simple-todos.js
+++ simple-todos.js
@@ -48,6 +48,15 @@ if( Meteor.isClient ) {
     },
     'click .delete': function() {
         Meteor.call( 'deleteTask', this._id );
+    },
+    'click .toggle-private': function() {
+      Meteor.call( 'setPrivate', this._id, !this.private );
+    }
+  });
+
+  Template.task.helpers({
+    isOwner: function() {
+      return this.owner === Meteor.userId();
     }
   });
 
@@ -69,17 +78,36 @@ Meteor.methods({
     });
   },
   deleteTask: function( taskId ) {
+    var task = Tasks.findOne( taskId );
+    if( task.private && task.owner !== Meteor.userId() )
+      throw new Meteor.Error( 'not-authorized' );
     Tasks.remove( taskId );
   },
   setChecked: function( taskId, setChecked ) {
+    var task = Tasks.findOne( taskId );
+    if( task.private && task.owner !== Meteor.userId() )
+      throw new Meteor.Error( 'not-authorized' );
     Tasks.update( taskId, {
       $set: { checked: setChecked }
+    });
+  },
+  setPrivate: function( taskId, setToPrivate ) {
+    var task = Tasks.findOne( taskId );
+    if( task.owner !== Meteor.userId() )
+      throw new Meteor.Error( 'not-authorized' );
+    Tasks.update( taskId, {
+      $set: { private: setToPrivate }
     });
   }
 });
 
 if( Meteor.isServer ) {
   Meteor.publish( 'tasks', function() {
-    return Tasks.find();
+    return Tasks.find({
+      $or: [
+        { private: { $ne: true } },
+        { owner: this.userId }
+      ]
+    });
   });
 }
